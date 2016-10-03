0ed2dd3ed8115ca1f7fe31ea5b8ea12592f52016
Magicode <herbmcsherb@gmail.com>
2016-10-03 19:13:30 -0400

    Create variable_practice.swift
    Different Variable defining processes and how to use them. Will update as I learn more ;)

---
+++ variable_practice.swift
@@ -0,0 +1,8 @@
+//Define a Variable
+var myVariable: Int
+//Initiate a Variable
+var myVariable = 12
+//Define and Initiate a Variable
+var myVariable: Int = 24
+//Using Swift's inference method to skip the Define process
+var aVariable = 64
