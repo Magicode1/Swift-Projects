7c9642774ed70f67a9a35f8e198fd0c52c1d2bd5
Magicode <herbmcsherb@gmail.com>
2016-10-05 09:59:55 -0400

    Create basicmath.swift
    Haven't had time to test it yet. Will when I get home!

---
+++ basicmath.swift
@@ -0,0 +1,17 @@
+print("Basic Sum-Finder!"
+
+//Input Code
+print("Please enter your 1st Number: ", terminator: "")
+let number1 = readLine()!
+print("Please enter your 1st Number: ", terminator: "")
+let number2 = readLine()!
+
+print("Here is the sum:")
+print(number1 + number2)
+
+
+
+
+
+
+
